d73af76bc88ddfe94abc287a83e0181a42f13b8e
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-05-17 12:22:12 +0200

    Change test

--- test/run.jl
+++ test/run.jl
@@ -7,7 +7,6 @@ max_coord = 2 - 2*eps(Float64)
 width = max_coord - min_coord
 
 pointlist = min_coord .+ rand(Float64, 2, 64) .* width
-scatter(pointlist)
-savefig("pointlist.png")
-display(pointlist)
-hull = NaturalNeighborsInterpolation.interpolate(pointlist)
+pointlist[:, 1] .= [1.501, 1.5]
+values = [pointlist[1,i] + pointlist[2,i] for i in 1:size(pointlist)[2]]
+hull = NaturalNeighborsInterpolation.interpolate(pointlist, values, Point(1.5, 1.5))
