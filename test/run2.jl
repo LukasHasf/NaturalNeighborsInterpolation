24492e277aeb797eb6eeec8ec241be50ff04c52e
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-05-20 14:34:05 +0200

    Update example

--- test/run2.jl
+++ test/run2.jl
@@ -16,8 +16,7 @@ end
 pointlist = [0.45 0.6 0.4 0.6 0.4 0.6 0.55 0.7  0.4; 
              0.4  0.4 0.6 0.6 0.55 0.5 0.4  0.55 0.4] .+ 1
 values = [pointlist[1,i] + pointlist[2,i] for i in 1:size(pointlist)[2]]
-points = Point2D[Point(pointlist[1,i], pointlist[2,i]) for i in 1:size(pointlist)[2]]
-intp = NaturalNeighborsInterpolator(points, values)
+intp = NaturalNeighborsInterpolator(pointlist, values)
 
 xs = 1.1:0.01:1.9
 ys = 1.1:0.01:1.9
@@ -26,12 +25,12 @@ interpolated_grid = interpolate_grid(xs, ys, intp)
 heatmap(interpolated_grid)
 savefig("interpolated_grid.png")
 
-intp_natural = NaturalNeighborsInterpolator(points, values, fallback="natural")
+intp_natural = NaturalNeighborsInterpolator(pointlist, values, fallback="natural")
 interpolated_grid = interpolate_grid(xs, ys, intp_natural)
 heatmap(interpolated_grid)
 savefig("interpolated_grid_natural.png")
 
-intp_natural = NaturalNeighborsInterpolator(points, values, fallback="nan")
+intp_natural = NaturalNeighborsInterpolator(pointlist, values, fallback="nan")
 interpolated_grid = interpolate_grid(xs, ys, intp_natural)
 heatmap(interpolated_grid)
 savefig("interpolated_grid_nan.png")
