7b4c13ed8697562f5db8f9720db931a1746b9d70
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-05-20 14:54:06 +0200

    Update example

--- test/run2.jl
+++ test/run2.jl
@@ -2,16 +2,6 @@ using NaturalNeighborsInterpolation
 using VoronoiDelaunay
 using Plots
 
-function interpolate_grid(xs, ys, intp)
-    interpolated_grid = Array{Float64, 2}(undef, length(ys), length(xs))
-    for j in eachindex(xs)
-        for i in eachindex(ys)
-            interpolated_grid[i,j] = intp(Point(xs[j],ys[i]))
-        end
-    end
-    return interpolated_grid
-end
-
 #pointlist = min_coord .+ rand(Float64, 2, 128) .* width
 pointlist = [0.45 0.6 0.4 0.6 0.4 0.6 0.55 0.7  0.4; 
              0.4  0.4 0.6 0.6 0.55 0.5 0.4  0.55 0.4] .+ 1
@@ -21,16 +11,16 @@ intp = NaturalNeighborsInterpolator(pointlist, values)
 xs = 1.1:0.01:1.9
 ys = 1.1:0.01:1.9
 
-interpolated_grid = interpolate_grid(xs, ys, intp)
+interpolated_grid = interpolateGrid(xs, ys, intp)
 heatmap(interpolated_grid)
 savefig("interpolated_grid.png")
 
 intp_natural = NaturalNeighborsInterpolator(pointlist, values, fallback="natural")
-interpolated_grid = interpolate_grid(xs, ys, intp_natural)
+interpolated_grid = interpolateGrid(xs, ys, intp_natural)
 heatmap(interpolated_grid)
 savefig("interpolated_grid_natural.png")
 
 intp_natural = NaturalNeighborsInterpolator(pointlist, values, fallback="nan")
-interpolated_grid = interpolate_grid(xs, ys, intp_natural)
+interpolated_grid = interpolateGrid(xs, ys, intp_natural)
 heatmap(interpolated_grid)
 savefig("interpolated_grid_nan.png")
